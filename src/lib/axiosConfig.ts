04bb110f387497609c3c831eb460ba0d0f9d7f07
unchhohang <unchhohang@gmail.com>
2025-11-25 14:03:05 +0545

    webhook test credentials to false

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -6,6 +6,6 @@ const ACCESS_TOKEN = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 // You can use an env variable here too
 axios.defaults.baseURL =
   'https://brantlee-nondegrading-shrinkingly.ngrok-free.dev/';
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 export default axios;
